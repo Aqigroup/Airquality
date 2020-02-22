940bba4b99fafb4441c93aab6d4ee6c2a3e9c2e6
Aqigroup <58731331+Aqigroup@users.noreply.github.com>
2020-02-22 11:23:02 +0530

    update
    database query issue

--- add_data.php
+++ add_data.php
@@ -8,7 +8,7 @@ $lat = $_GET['lat'];
 $lgt = $_GET['lgt'];
 $aqi = $_GET['aqi'];
 
-$q = "INSERT INTO aqi (id, lat, lgt, aqi) VALUES ('$id', '$lat', '$lgt', '$aqi')";
+$q = "INSERT INTO `Air Quality Index` (id, lat, lgt, aqi) VALUES ('$id', '$lat', '$lgt', '$aqi')";
 
 $r = @mysqli_query($dbc, $q); // Run the query.
 
